a3594646ced3a7cd6b2c8e9809721f1c88342aaf
TechOtakupoi233 <slkj104@gmail.com>
2023-08-28 01:26:38 +0800

    1.0 release coomit

--- Mona/Data/Mona_TradeScience.lua
+++ Mona/Data/Mona_TradeScience.lua
@@ -2,7 +2,7 @@ include("GameCapabilities");
 GAME_SPEED = GameConfiguration.GetGameSpeedType()
 GAME_SPEED_MULTIPLIER = GameInfo.GameSpeeds[GAME_SPEED] and GameInfo.GameSpeeds[GAME_SPEED].CostMultiplier / 100 or 1
 
-DebugON = true
+-- DebugON = true
 -- 回合计数器
 function TurnCounter()
     -- 获取当前回合数，if回合数能被5整除
@@ -49,69 +49,11 @@ function MonaTradeScience(MonaIDs, HighestSciencePlayerID)
         -- 飘字文本
         MonaCapitalText = '[COLOR_FLOAT_GOLD]' .. string.format("%d", TradingGold) .. GameInfo.Yields['YIELD_GOLD'].IconString .. '[ENDCOLOR]' .. '[ICON_GoingTo]'.. '[COLOR_FLOAT_SCIENCE]' ..  string.format("%d", TradingScience) .. GameInfo.Yields['YIELD_SCIENCE'].IconString .. '[ENDCOLOR]'
         CapitalWorldViewTextSender(playerID, MonaCapitalText)
-        TradeScienceCalc(pPlayer, TradingScience)
         -- 转账到除自己外科技值最高的玩家
         Players[HighestSciencePlayerID]:GetTreasury():ChangeGoldBalance(TradingGold)
         if DebugON then print("Sending " .. TradingGold .. " Gold to Player " .. HighestSciencePlayerID) end
         HighestSciencePlayerCapitalText = '[COLOR_FLOAT_GOLD]' .. '+' .. string.format("%.1f", TradingGold) .. GameInfo.Yields['YIELD_GOLD'].IconString .. '[ENDCOLOR]'
         CapitalWorldViewTextSender(HighestSciencePlayerID, HighestSciencePlayerCapitalText)
-        end
-    end
-end
-
--- 累积奖励计算
-function TradeScienceCalc(pPlayer, TradingScience)
-    -- 初始化存档数据
-    if pPlayer:GetProperty("SUM.MonaTradeSciene") == nil then
-        pPlayer:SetProperty("SUM.MonaTradeScience", 0)
-        pPlayer:SetProperty("sValue.MonaTradeScience", 0)
-        pPlayer:SetProperty("gValue.MonaTradeScience", 0)
-        if DebugON then print("Initiating PlayerProperties: " .. pPlayer:GetProperty("SUM.MonaTradeSciene")) end
-    end
-    -- 更新本回合数据
-    local SUM, sValue, gValue = pPlayer:GetProperty("SUM.MonaTradeSciene"), pPlayer:GetProperty("sValue.MonaTradeSciene"), pPlayer:GetProperty("gValue.MonaTradeSciene")
-    pPlayer:SetProperty("SUM.MonaTradeScience", SUM + TradingScience)
-    pPlayer:SetProperty("sValue.MonaTradeScience", sValue + TradingScience)
-    pPlayer:SetProperty("gValue.MonaTradeScience", gValue + TradingScience * 2)
-    local sValue, gValue = pPlayer:GetProperty("sValue.MonaTradeSciene"), pPlayer:GetProperty("gValue.MonaTradeSciene")
-    -- 每100科技值为玩家附加一个加星纬殿堂产出的Modifier(+1科技)
-    if sValue >= 100 then repeat sValue = sValue - 100
-        pPlayer:AttachModifierByID(GameInfo.Modifiers["MOD_STELLARIS_PHANTASM_SCIENCE"].ModifierId)
-        until sValue < 100
-        pPlayer:SetProperty("sValue.MonaTradeScience", sValue)
-    end
-    MonaAutoBuyCampusBuildings(pPlayer, gValue)
-end
-
--- 自动购置学院区建筑
-function MonaAutoBuyCampusBuildings(pPlayer, gValue)
-    -- 从数据库获取建筑原价并计算游戏速度影响
-    bLib, cLib = GameInfo.Buildings["BUILDING_LIBRARY"].Index, GameInfo.Buildings["BUILDING_LIBRARY"].Cost * GAME_SPEED_MULTIPLIER
-    bUni, cUni = GameInfo.Buildings["BUILDING_UNIVERSITY"].Index, GameInfo.Buildings["BUILDING_UNIVERSITY"].Cost * GAME_SPEED_MULTIPLIER
-    bRes, cRes = GameInfo.Buildings["BUILDING_RESEARCH_LAB"].Index, GameInfo.Buildings["BUILDING_RESEARCH_LAB"].Cost * GAME_SPEED_MULTIPLIER
-    local pCities = pPlayer:GetCities();
-    local pCity;
-    for ii, pCity in pCities:Members() do
-        if (pCity ~= nil) then
-            local pCityBuildings = pCity:GetBuildings();
-            local pCityBuildQueue = pCity:GetBuildQueue();
-            -- 如果城市没有建筑且累积金币足够则购买
-            if (not pCityBuildings:HasBuilding(bLib)) and gValue >= cLib then
-                pCityBuildQueue:CreateIncompleteBuilding(bLib);
-                gValue = gValue - cLib
-                if DebugON then print("Bought a Library in " .. pCity:GetName(pCity)) end
-                return gValue
-            elseif (not pCityBuildings:HasBuilding(bUni)) and gValue >= cUni then
-                pCityBuildQueue:CreateIncompleteBuilding(bUni);
-                gValue = gValue - cUni
-                if DebugON then print("Bought a University in " .. pCity:GetName(pCity)) end
-                return gValue
-            elseif (not pCityBuildings:HasBuilding(bRes)) and gValue>= cRes then
-                pCityBuildQueue:CreateIncompleteBuilding(bRes);
-                gValue = gValue - cRes
-                if DebugON then print("Bought a Research Lab in " .. pCity:GetName(pCity)) end
-                return gValue
-            end
         end
     end
 end
